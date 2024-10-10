e33cd684f13715962b7bdbb33c695487b4bcb18b
sanermachado <sanermachado@hotmail.com>
2024-10-10 11:08:54 -0300

    feat : ajustes finais

--- .idea/src/scripts/main.js
+++ .idea/src/scripts/main.js
@@ -1,11 +1,10 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Capturar os valores dos campos de e-mail e senha
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
 
-    // Exemplo de validação simples (somente para efeito de demonstração)
+    // valida se esta ok
     if (email === "exemplo@gmail.com" && senha === "0123456789") {
         alert("Login realizado com sucesso!");
     } else {
